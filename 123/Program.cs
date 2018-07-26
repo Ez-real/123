7f80e4ab84b99e957227bfa11979c1f6aea3e7f5
Ming <779715566@qq.com>
2018-07-26 08:53:14 +0800

    tym

--- 123/Program.cs
+++ 123/Program.cs
@@ -23,6 +23,15 @@ namespace _123
                 i++;
             } while (i < myArray.Length);//设置do…while语句的条件
             Console.ReadLine();
+
+            Console.WriteLine("tym");
+            string[] str = { "a", "b", "c", "d", "e", "f" };
+            for (int i = 0; i < str.Length / 2; i++)
+            {
+                string temp = str[i];
+                str[i] = str[str.Length - 1 - i];
+                str[str.Length - 1 - i] = temp;
+            }
         }
     }
 }
