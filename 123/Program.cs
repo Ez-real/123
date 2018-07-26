5e152180c474e245a35c009f0850e40db848bc16
493028713 <493028713@qq.com>
2018-07-26 08:45:11 +0800

    肖蕾

--- 123/Program.cs
+++ 123/Program.cs
@@ -11,6 +11,18 @@ namespace _123
         static void Main(string[] args)
         {
 
+
+
+
+
+            string[] myArray = new string[3] { "世界杯", "欧洲杯", "欧冠" };//声明一个string数组并初始化
+            int i = 0;
+            do//调用do…while语句
+            {
+                Console.WriteLine(myArray[i]);//输出数组中数据
+                i++;
+            } while (i < myArray.Length);//设置do…while语句的条件
+            Console.ReadLine();
         }
     }
 }
